63a28ad9ebee72a9fcdb2bedbf218af414397ced
rahman <rahmannurhayadi@gmail.com>
2025-04-24 19:14:45 +0700

    feat(layout) : add main layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="font-rubik">
+        <HeroPage />
+        <CategoryPage />
+        <TrendingPage />
+    </div>
+</template>
+
+<script setup>
+import HeroPage from '@/components/pages/HeroPage.vue'
+import CategoryPage from '@/components/pages/CategoryPage.vue';
+import TrendingPage from '@/components/pages/TrendingPage.vue';
+</script>
