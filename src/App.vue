f3b707ed70850bfaeb1128d33820a295eb5e28d9
rahman <rahmannurhayadi@gmail.com>
2025-04-24 19:15:00 +0700

    fix(app) : fix app layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
+  <!-- Navigation -->
   <NavBar />
+  <!-- Main Content -->
   <main>
     <MainLayout />
   </main>
+  <!-- Mobile Bottom Navigation -->
+  <MobileBottomNav />
+  <!-- Footer -->
+  <FooterSection />
 </template>
 
 <script setup>
 import NavBar from './components/navigation/NavBar.vue';
+import MobileBottomNav from './components/navigation/MobileBottomNav.vue';
 import MainLayout from './layouts/MainLayout.vue'
+import FooterSection from './components/pages/FooterSection.vue';
 </script>
