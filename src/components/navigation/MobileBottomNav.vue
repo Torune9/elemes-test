4e75635bc532fe2fa8ae7210406c713ba4bc4f9f
rahman <rahmannurhayadi@gmail.com>
2025-04-24 19:09:51 +0700

    style(compoent) : delete gap other menu

--- src/components/navigation/MobileBottomNav.vue
+++ src/components/navigation/MobileBottomNav.vue
@@ -1,24 +1,31 @@
 <template>
-    <div class="w-full fixed flex flex-row bottom-0 justify-around text-gray-600 md:hidden bg-soft-ivory">
-        <button class="cursor-pointer hover:text-gray-700 transition-colors duration-500">
-            <i class="pi pi-home" style="font-size: 2rem"></i>
-            <p>Home</p>
-        </button>
-        <button class="cursor-pointer hover:text-gray-700 transition-colors duration-500">
-            <i class="pi pi-ticket" style="font-size: 2rem"></i>
-            <p>Promotions</p>
-        </button>
-        <button class="cursor-pointer hover:text-gray-700 transition-colors duration-500" @click="showOtherMenu">
-            <i class="pi pi-bars" style="font-size: 2rem"></i>
-            <p>Other</p>
-        </button>
-        <!-- Other Menu -->
-        <div class="absolute -translate-y-full right-0 flex flex-col gap-y-4 text-lg bg-soft-white w-40 py-2"
-            v-if="isOtherMenu">
-            <button class="py-2 cursor-pointer hover:bg-soft-ivory border-b">Masuk</button>
-            <button class="py-2 cursor-pointer hover:bg-soft-ivory border-b">About</button>
-            <button class="py-2 cursor-pointer hover:bg-soft-ivory border-b">Blogs</button>
-            <button class="py-2 cursor-pointer hover:bg-soft-ivory border-b">Contact Us</button>
+    <div class="fixed bottom-0 w-full">
+        <div class="w-full flex flex-row justify-around text-medium-gray lg:hidden bg-soft-white py-2">
+            <button class="cursor-pointer hover:text-gray-700 transition-colors duration-500">
+                <i class="pi pi-home" style="font-size: 2rem"></i>
+                <p>Home</p>
+            </button>
+            <button class="cursor-pointer hover:text-gray-700 transition-colors duration-500">
+                <i class="pi pi-ticket" style="font-size: 2rem"></i>
+                <p>Promotions</p>
+            </button>
+            <button class="cursor-pointer hover:text-gray-700 transition-colors duration-500" @click="showOtherMenu">
+                <i class="pi pi-bars" style="font-size: 2rem"></i>
+                <p>Other</p>
+            </button>
+            <!-- Other Menu -->
+            <div class="absolute -translate-y-full right-0 flex flex-col  text-lg bg-soft-white w-40"
+                v-if="isOtherMenu">
+                <button
+                    class="py-2 cursor-pointer hover:bg-slate-400 hover:text-white transition-colors duration-300 border-b">Masuk</button>
+                <button
+                    class="py-2 cursor-pointer hover:bg-slate-400 hover:text-white transition-colors duration-300 border-b">About</button>
+                <button
+                    class="py-2 cursor-pointer hover:bg-slate-400 hover:text-white transition-colors duration-300 border-b">Blogs</button>
+                <button
+                    class="py-2 cursor-pointer hover:bg-slate-400 hover:text-white transition-colors duration-300 border-b">Contact
+                    Us</button>
+            </div>
         </div>
     </div>
 </template>
