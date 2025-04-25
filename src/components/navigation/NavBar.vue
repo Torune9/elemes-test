845284a406a4b73f787b200cd06f4a0f95f54d48
rahman <rahmannurhayadi@gmail.com>
2025-04-25 07:24:31 +0700

    style(component) : add hot indicator in promotion menu

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="font-rubik sticky top-0 z-50 bg-white">
+    <nav class="font-rubik sticky top-0 z-50 bg-white py-4">
         <!-- Wrapper Dekstop Navigation -->
         <div class="p-4 flex flex-row justify-around">
             <!-- Logo -->
@@ -10,11 +10,13 @@
             <ul class="flex flex-row gap-x-8 items-center max-lg:hidden">
                 <!--Child Menu -->
                 <li v-for="(menu, i) in menus"
-                    class="cursor-pointer text-gray-600 group hover:bg-soft-white transition-colors duration-500 p-1"
+                    class="cursor-pointer text-gray-600 group hover:bg-soft-white transition-colors duration-500 p-1 relative"
                     :key="i">
                     <p>
                         {{ menu.name }}
                     </p>
+                    <span v-if="menu.name == 'Promotions'"
+                        class="absolute border top-0 left-3/4 -translate-y-3/5 inline-block w-12 text-center rounded-xl text-xs bg-red-600 text-white py-1">HOT</span>
                 </li>
             </ul>
             <!-- Auth Action -->
